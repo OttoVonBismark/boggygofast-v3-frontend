96bc93e71671860fb5ad72689ace74531a7db6ff
Aaron Zaborac <aaron.zaborac@outlook.com>
2021-08-20 07:53:35 -0400

    Makes success/fail state when confirming email more clear in UI.

--- src/containers/users/Confirm.js
+++ src/containers/users/Confirm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 // import { notify } from 'react-notify-toast'
 import axios from 'axios'
@@ -9,21 +9,36 @@ const Confirm = (props) => {
   const BASE_URL = 'http://localhost:3001/confirmation?confirmation_token='
   const { token } = useParams()
 
+  const [confirmed, setConfirmed] = useState([])
+
   useEffect(() => {
+    const getEmailConfirmation = async () => {
+      const res = await confirmEmail()
+      console.log(res)
+    }
+
+    getEmailConfirmation()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [token])
+
+  const confirmEmail = async () => {
     axios.get(`${BASE_URL}${token}`)
-    .catch(
-      data => {
-        if (data.response.status === 400) {
-          props.history.push('/404')
+      .catch(
+        data => {
+          if (data.response.status === 400) {
+            setConfirmed('Heck!')
+            console.log('Error', data)
+          }
+          if (data.response.status === 200) {
+            setConfirmed('Success!')
+          }
         }
-        console.log('Error', data)
-      }
-    )
-  }, [])
+      )
+  }
 
   return (
     <div className='confirm'>
-      <h1>Confirming... done.</h1>
+      <h1 className="font-bold text-2xl">Confirming email... {confirmed}</h1>
     </div>
   )
 }
