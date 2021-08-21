0bf00b34f283e3b87889a0781aae9fd2d990fb3e
Aaron Zaborac <raegaltboggart@hotmail.com>
2021-08-20 20:15:48 -0400

    Refactors CSS framework

--- src/components/layout/NavItem.js
+++ src/components/layout/NavItem.js
@@ -2,9 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NavItem = ({ text, url }) => {
-  // TODO: Rename CSS classes when we get back to working on this.
   return (
-    <Link className="cerb-nav-item" to={url}>{text}</Link>
+    <Link className="bgf-nav-item" to={url}>{text}</Link>
   )
 }
 
